3978ca934ec5b5da9b846a9a1710015815b02e82
Felix <stop.pfr@gmail.com>
2017-12-08 22:37:43 -0600

    Cambios utiles para conexion con Firebase

--- src/pages/detalles/detalles.ts
+++ src/pages/detalles/detalles.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
+import { IonicPage, NavController, NavParams } from 'ionic-angular';/**
  * Generated class for the DetallesPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
@@ -16,7 +14,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class DetallesPage {
 
   alimento: any ={}
-  
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 
     console.log(navParams);
